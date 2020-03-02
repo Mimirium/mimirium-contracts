8af3a7b9a6f062abdd3553d11697ef2f243fd700
Galin Dinkov <galin@mimirium.io>
2020-03-02 18:13:35 +0200

    - Solidity version changed - DataGenerator added

--- contracts/CampaignRegister.sol
+++ contracts/CampaignRegister.sol
@@ -56,17 +56,17 @@ contract CampaignRegister is Versionable, Ownable {
     function createCampaign(
         string memory _multihash,
         DataTypes _dataType,
-        bytes32 _company, 
-        uint256 _minRespondents, 
-        uint256 _maxRespondents, 
-        uint256 _startTime, 
-        uint256 _endTime) 
-        public payable onlyOwner 
-        returns(bytes32) {        
+        bytes32 _company,
+        uint256 _minRespondents,
+        uint256 _maxRespondents,
+        uint256 _startTime,
+        uint256 _endTime)
+        public payable onlyOwner
+        returns(bytes32) {  
         require(companyReg.companyExists(_company), "This company is not registered");
         require(_endTime > _startTime, "endTime must be after startTime");
         require(_startTime >= now, "Campaigns cannot be in the past");
-        require(msg.value > 0, "Give some cash");        
+        require(msg.value > 0, "Give some cash");
 
         bytes32 id = generateUniqueId();
         Campaign memory c = Campaign(id, _multihash, _dataType, _company, _minRespondents, _maxRespondents, msg.value, _startTime, _endTime);
